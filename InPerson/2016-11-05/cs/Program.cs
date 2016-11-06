8c737af2c5b87a40960a2b3764c07de4d506a35a
Navneet Gupta <Navneet Gupta>
2016-11-06 01:13:49 -0700

    Max Width

--- InPerson/2016-11-05/cs/Program.cs
+++ InPerson/2016-11-05/cs/Program.cs
@@ -4,148 +4,66 @@ using System.Linq;
 
 namespace ConsoleApplication
 {
-    //Given a Binary Tree find the length of the longest path which comprises of nodes with consecutive values in increasing order. Every node is considered as a path of length 1.
-    public class Program
+    //Given a binary tree, write a function to get the maximum width of the given tree. Width of a tree is maximum of widths of all levels.
+    public partial class Program
     {
         public static void Main(string[] args)
         {
             Dictionary<int,int> tree = new Dictionary<int,int>();
-            tree.Add(0, 10);
-            tree.Add(1, 11);
-            tree.Add(2, 8);
-            tree.Add(3, 13);
-            tree.Add(4, 12);
-            tree.Add(5, 13);
+            tree.Add(0, 1);
+            tree.Add(1, 2);
+            tree.Add(2, 3);
+            tree.Add(3, 4);
+            tree.Add(4, 5);
             tree.Add(6, 9);
             tree.Add(13, 10);
-            tree.Add(27, 11);
+            tree.Add(14, 11);
 
-           int[] result = LongestConsecutiveSequence(tree);
-           Print(result);
+           int result = MaxWidth(tree);
+           Console.WriteLine(result);
 
         }
-
-        private static int[] LongestConsecutiveSequence(Dictionary<int,int> tree)
+        private static int MaxWidth(Dictionary<int,int> tree) 
         {
-            var results = LongestConsecutiveSequence(tree, 0);
-            var result = Max(results);
-            
-            return Enumerable.Range(result.startValue, result.length).ToArray(); 
+            if(tree.Count == 0) 
+            {
+                return 0;
+            }
+            List<int> nodes = new List<int>();
+            nodes.Add(0);
+            return MaxWidth(tree, nodes);
         }
 
-        private static Result Max(List<Result> results)
+        private static int MaxWidth(Dictionary<int,int> tree, List<int> nodes)
         {
-            if(!results.Any())
+            if(nodes.Count == 0)
             {
-                return null;
+                return 0;
             }
-            int max = results[0].length;
-            Result candidate = results[0];
-            for(int i =1;i<results.Count;i++) {
-                if(max < results[i].length) {
-                    max = results[i].length;
-                    candidate = results[i];
-                }
+            List<int> nextLevel = new List<int>();
+            foreach(var index in nodes)
+            {
+                nextLevel.AddRange(GetChildIndexs(tree, index));
             }
-            return candidate;
+            return Math.Max(nodes.Count, MaxWidth(tree, nextLevel));
         }
 
-        private static List<Result> LongestConsecutiveSequence(Dictionary<int,int> tree, int index)
+        private static List<int> GetChildIndexs(Dictionary<int,int> tree, int index)
         {
-            List<Result> results = new List<Result>();
-
-            if(!tree.ContainsKey(index))
+            int leftChild = index *2 +1;
+            int rightChild = index * 2 +2;
+            List<int> result = new List<int>();
+            if(tree.ContainsKey(leftChild))
             {
-                return results;
+                result.Add(leftChild);
             }
 
-            int leftIndex = (index *2)+1;
-            int rightIndex = (index *2)+2;
-            var leftResult = LongestConsecutiveSequence(tree, leftIndex);
-            var rightResult = LongestConsecutiveSequence(tree, rightIndex);
-            
-            var maxLeft = Max(leftResult);
-            var maxRight = Max(rightResult);
-            var includedCurrent = false;
-            if(maxLeft != null)
+            if(tree.ContainsKey(rightChild))
             {
-                if(maxLeft.startIndex == leftIndex && maxLeft.startValue == tree[index]+1)
-                {
-                    maxLeft.startIndex = index;
-                    maxLeft.startValue = tree[index];
-                    maxLeft.length = maxLeft.length+1;
-                    results.Add(maxLeft);
-                    includedCurrent = true;
-                }
-                else
-                {
-                    results.Add(maxLeft);
-                }
+                result.Add(rightChild);
             }
 
-            if(maxRight != null)
-            {
-                if(maxRight.startIndex == rightIndex && maxRight.startValue == tree[index]+1)
-                {
-                    maxRight.startIndex = index;
-                    maxRight.startValue = tree[index];
-                    maxRight.length = maxRight.length+1;
-                    results.Add(maxRight);
-                    includedCurrent = true;
-                }
-                else
-                {
-                    results.Add(maxRight);
-                }
-            }
-
-            //if the current node is not included then if either of left or right node contains any sequence than take it
-            if(includedCurrent == false)
-            {
-                 Result current = new Result() {
-                     startIndex = index,
-                     startValue = tree[index],
-                     length = 1
-                 };
-
-                 List<Result> temp = new List<Result>();
-                 temp.Add(current);
-                 var next = leftResult.FirstOrDefault(lr => lr.startIndex == index+1 && lr.startValue == tree[index] +1);
-                 if(next != null)
-                 {
-                     next.startIndex = index;
-                     next.startValue = tree[index];
-                     next.length = next.length +1;
-                     temp.Add(next);
-                 }
-                 next = rightResult.FirstOrDefault(lr => lr.startIndex == index+1 && lr.startValue == tree[index] +1);
-                 if(next != null)
-                 {
-                     next.startIndex = index;
-                     next.startValue = tree[index];
-                     next.length = next.length +1;
-                     temp.Add(next);
-                 }
-                 var max = Max(temp);
-                 results.Add(max);
-            }
-
-            return results;
+            return result;
         }
-
-        
-        private static void Print(int[] arr)
-        {
-            string x = ",";
-           Console.WriteLine($"[{string.Join(x, arr)}]");
-        }
-    }
-    class Result
-    {
-       public int startIndex;
-       public int length;
-       public int startValue;
-    }
-    //Given a Binary Tree find the length of the longest path which comprises of nodes with consecutive values in increasing order. Every node is considered as a path of length 1.
-    
+    }    
 }
